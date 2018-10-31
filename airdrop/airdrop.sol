27c1d1b7ea86b9a6bffdeb0534dad075d16ccc65
Avinash <avinash975@gmail.com>
2018-10-31 23:15:23 +0530

    removed the ownable contract

--- airdrop/airdrop.sol
+++ airdrop/airdrop.sol
@@ -1,39 +1,26 @@
-contract Ownable {
-
-  address public owner;
-
-  function Ownable() {
-    owner = msg.sender;
-  }
-
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-}
+pragma solidity ^0.4.24;
 
 interface Token {
   function transfer(address _to, uint256 _value) external returns (bool);
   function balanceOf(address _owner) external constant returns (uint256 balance);
 }
 
-contract AirDrop is Ownable {
+contract AirDrop  {
 
   Token token;
-
+ 
+  function multisend(address _tokenAddr, address[] _recipient, uint256 _value)  external {
     
-  function multisend(address _tokenAddr, address[] _recipient, uint256 _value)  onlyOwner external {
-    
-    token = Token(_tokenAddr);
+   // token = Token(_tokenAddr);
     for (uint256 i = 0; i < _recipient.length; i++) {
         
-        if(token.balanceOf(_tokenAddr) >= _value) {
-           token.transfer(_recipient[i], _value);
+        if(Token(_tokenAddr).balanceOf(msg.sender) >= _value) {
+           Token(_tokenAddr).transfer(_recipient[i], _value);
            
         } 
           
         } 
   }  
+  
 
 }
